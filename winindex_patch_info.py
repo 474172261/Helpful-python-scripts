a242923fd9c0736719f04f07807f2480f66808d8
VictorV <vv474172261@hotmail.com>
2023-04-03 15:56:15 +0800

    Add files via upload

---
+++ winindex_patch_info.py
@@ -0,0 +1,168 @@
+import sys
+from bs4 import BeautifulSoup
+import requests
+import gzip
+import urllib.request
+import requests
+import json
+import os
+import wget
+
+file = "vmbusr.sys"
+URL = "https://winbindex.m417z.com/?file="
+def get_file_info(file, keyword, timerange = (0, 20250000)):
+    filename = "D:\\tmp\\tmp_index\\"+file+".json.gz"
+
+    if not os.path.exists(filename):
+        req = "https://winbindex.m417z.com/data/by_filename_compressed/"+file.lower()+".json.gz"
+        print("downloading {}".format(req));
+        try:
+            # response = urllib.request.urlopen(req)
+            response = requests.get(req)
+            if response.text.startswith('<!DOCTYPE html>'):
+                print("no such file")
+                return []
+            compressed_file = response.content
+            with open(filename,"wb") as f:
+                f.write(compressed_file)
+        except:
+            print("[Error] fail to get this file")
+            return []
+
+
+    with gzip.open(filename, 'rb') as f:
+        file_content = f.read()
+
+    data = json.loads(file_content)
+
+    result = {}
+    for key, value in data.items():
+        # print(key, type(value), value)
+        if value.get('fileInfo'):
+            timestamp = value.get('fileInfo')['timestamp']
+            try:
+                virtualSize = value.get('fileInfo')['virtualSize']
+            except:
+                virtualSize = 0
+        else:
+            timestamp = 0
+            virtualSize = 0
+        windows_versions = value.get('windowsVersions')
+        if windows_versions:
+            tmp = {}
+            first_windows_version = list(windows_versions.keys())[0]
+            for k, v in windows_versions[first_windows_version].items():
+                if k.startswith('KB'):
+                    tmp['KB'] = k
+                    release_date = v.get('updateInfo', {}).get('releaseDate')
+                    tmp['release_date'] = release_date
+                    tmp['release_date_int'] = int(release_date.replace('-',''))
+                    tmp['timestamp'] = timestamp
+                    tmp['virtualSize'] = virtualSize
+                    if virtualSize:
+                        tmp['file_id'] ='{:08X}{:X}'.format(timestamp, virtualSize)
+                    break
+            if first_windows_version not in result:
+                result[first_windows_version] = [tmp]
+            else:
+                result[first_windows_version].append(tmp)
+    item_ret = []
+    for item in result.keys():
+        if keyword in item:
+            item_ret = result[item]
+            break
+    
+    ret = []
+    for each in item_ret:
+        if each == {}:
+            return []
+        try:
+            if timerange[0] < each['release_date_int'] < timerange[1]:
+                ret.append(each)
+        except:
+            print(each, item_ret)
+            print("exit")
+            exit(0)
+    return ret
+
+
+def get_download_link(peName, fileId):
+    return 'https://msdl.microsoft.com/download/symbols/' + peName + '/' + fileId + '/' + peName;
+
+main_lists = ["vmswitch.sys", 'vmsproxy.sys', 'storvsp.sys', 'passthruparser.sys',
+'vhdparser.sys', 'vpcivsp.sys', 'vmbkmclr.sys', 'vmbusr.sys', 'vid.sys', 'hvix64.exe',
+'winhvr.sys', 'vmwp.exe']
+part_list1 = ['pcip.sys', 'synth3dvsp.sys', 'ramparser.sys', 'vmsvcext.sys', 'lunparser.sys']
+
+all_list = ['hvsicontainerservice.dll', 'hvsifiletrust.dll', 'hvsimgrps.dll', 'hvsiofficeiconoverlayshellextension.dll', 'hvsicontainerservice.dll', 'hvsiDspdvcclient.dll', 'hvsirdpclient.exe', 'hvsifiletrust.dll', 'hvsimgr.exe', 'HvsiMachinePolicies.dll', 'hvsimgrps.dll', 'hvsiofficeiconoverlayshellextension.dll', 'HvsiSettingsProvider.dll', 'Provider.dll', 'hvsimgr.exe', 'hvsirdpclient.exe', 'HvsiSettingsWorker.exe', 'hvsicontainerservice.dll', 'hvsigpext.dll', 'AuditSettingsProvider.dll', 'madrid.dll', 'cmclient.dll', 'vmcompute.dll', 'vmcomputeeventlog.dll', 'VmComputeProxy.dll', 'hnsproxy.dll', 'HostNetSvc.dll', 'CmService.dll', 'computestorage.dll', 'NetMgmtIF.dll', 'NvAgent.dll', 'vmsif.dll', 'vmsifcore.dll', 'vmsifproxystub.dll', 'VmSynthNic.dll', 'gns.dll', 'vmdynmem.dll', 'vmflexio.dll', 'vmiccore.dll', 'vmpmem.dll', 'vmserial.dll', 'vmsmb.dll', 'vmsynthstor.dll', 'vmuidevices.dll', 'VrdUmed.dll', 'gpupvdev.dll', 'vmchipset.dll', 'ActivationVdev.dll', 'vmwp.exe', 'sbresources.dll', 'rdp4vs.dll', 'UtilityVmSysprep.dll', 'vmbuspiper.dll', 'vmbusvdev.dll', 'VmCrashDump.dll', 'vmprox.dll', 'vmusrv.dll', 'vmvirtio.dll', 'vmwpctrl.dll', 'vmwpevents.dll', 'vfpapi.dll', 'vp9fs.dll', 'vmsmb.dll', 'icsvc.dll', 'icsvcext.dll', 'vid.dll', 'winhvplatform.dll', 'winhvemulation.dll', 'ComputeLegacy.dll', 'ComputeStorage.dll', 'ComputeCore.dll', 'ComputeNetwork.dll', 'DeviceVirtualization.dll', 'ComputeNetwork.dll', 'rdvvmtransport.dll', 'RdvgmProxy.dll', 'vmstaging.dll', 'HyperVSysprepProvider.dll', 'hgattest.dll', 'hgsclientplugin.dll', 'hgclientservice.dll', 'hgclientserviceps.dll', 'hgsclientplugin.dll', 'HgsClientWmi.dll', 'HostGuardianServiceClientResources.dll', 'NetMgmtIF.dll', 'rdp4vs.dll', 'NvAgent.dll', 'rtpm.dll', 'RdvgmProxy.dll', 'RdvGpuInfo.dll', 'RemoteFileBrowse.dll', 'TpmEngUM.dll', 'synth3dvideoproxy.dll', 'vmstaging.dll', 'vmsynth3dvideo.dll', 'vmsynthfcvdev.dll', 'vmtpm.dll', 'VmDataStore.dll', 'vmdebug.dll', 'vpcievdev.dll', 'vmemulateddevices.dll', 'VmEmulatedNic.dll', 'VmEmulatedStorage.dll', 'vmhgs.dll', 'vmicrdv.dll', 'vsconfig.dll', 'vmicvdev.dll', 'vmmsprox.dll', 'utilityVid.sys', 'hvsifltr.sys', 'vmswitch.sys', 'VmsProxy.sys', 'VmsProxyHNic.sys', 'l2bridge.sys', 'vfpext.sys', 'NdisVirtualBus.sys', 'wcifs.sys', 'hvsocketcontrol.sys', 'hvsocket.sys', 'storvsp.sys', 'passthruparser.sys', 'vhdparser.sys', 'pvhdparser.sys', 'vpcivsp.sys', 'storvsc.sys', 'vmbkmclr.sys', 'vmbkmcl.sys', 'vmbusr.sys', 'vmbus.sys', 'VMBusHID.sys', 'hyperkbd.sys', 'HyperVideo.sys', 'winhvr.sys', 'winhv.sys', 'vid.sys', 'hvservice.sys', 'hvix64.exe', 'hvcrash.sys', 'vhdmp.sys', 'vmgencounter.sys', 'vmgid.sys', 'pcip.sys', 'Synth3dVsp.sys', 'ramparser.sys', 'vmsvcext.sys', 'lunparser.sys', 'vkrnlintvsc.sys', 'vkrnlintvsp.sys', 'hnsdiag.exe', 'hvsimgr.exe', 'hvsiproxyapp.exe', 'hvsirdpclient.exe', 'hvsirpcd.exe', 'hvsievaluator.exe', 'HvsiSettingsWorker.exe', 'wdagtool.exe', 'CExecSvc.exe', 'cmdiag.exe', 'cmimageworker.exe', 'vmcompute.exe', 'VmComputeAgent.exe', 'wcsetupagent.exe', 'hcsdiag.exe', 'vfpctrl.exe', 'nmbind.exe', 'nmscrub.exe', 'nvspinfo.exe', 'vmwp.exe', 'WindowsSandbox.exe', 'hvc.exe', 'hnsdiag.exe', 'rdvgm.exe', 'vmplatformca.exe', 'vmsp.exe', 'vmconnect.exe', 'vmms.exe', 'Hvsigpext.dll', 'rdbss.sys', 'wcifs.sys', 'ntoskrnl.exe', 'vmcompute.exe', 'VmComputeAgent.exe', 'CmService.dll', 'rdsdwmdr.dll', 'rdsxvmaudio.dll']
+
+except_list = ['computelegacy.dll', 'provider.dll', 'utilityvid.sys', 'devicevirtualization.dll', 'l2bridge.sys', 'vkrnlintvsp.sys', 'vmcomputeeventlog.dll']
+
+
+start_list = list(set(all_list))
+
+start_list = [i for i in start_list if i.lower() not in [x.lower() for x in except_list]]
+
+
+
+if 0:
+    print(get_file_info('vmchipset.dll', '1809', (20220600, 20220700)))
+    exit(0)
+
+def get_patch_info(winver, time_month):
+    time_start = time_month*100
+    result = []
+    for each in start_list:
+        ret = get_file_info(each, winver, (time_start, time_start+15))
+        if ret == []:
+            continue
+        result.append([each, ret])
+    return result
+
+def help():
+    print("""
+    this.py winver date
+        this.py 1809 202206
+    this.py winver date name folder
+        this.py 1809 202206 vmbkmclr.sys D:\\tmp\\""")
+
+def main():
+    if len(sys.argv) == 3:
+        winver = sys.argv[1]
+        time_month = int(sys.argv[2], 10)
+        result = get_patch_info(winver, time_month)
+        for each in result:
+            print(each[0])
+            print(each[1])
+    elif len(sys.argv) == 5:
+        winver = sys.argv[1]
+        time_month = int(sys.argv[2], 10)
+        time_start = time_month*100
+        target_file = sys.argv[3]
+        download_folder = sys.argv[4]
+        result = get_file_info(target_file, winver, (time_start-10000, time_start+15))
+        print(result[-1])
+        print(result[-2])
+        if result[-1]['file_id'] == result[-2]['file_id']:
+            print("same")
+            return
+        cur = result[-1]
+        link = get_download_link(target_file, cur['file_id'])
+        path = os.path.join(download_folder, cur['release_date']+'-'+target_file)
+        if not os.path.exists(path):
+            print('\nget', path)
+            wget.download(link, path)
+
+        cur = result[-2]
+        link = get_download_link(target_file, cur['file_id'])
+        path = os.path.join(download_folder, cur['release_date']+'-'+target_file)
+        if not os.path.exists(path):
+            print('\nget', path)
+            wget.download(link, path)
+    else:
+        help()
+
+
+        
+if __name__ == '__main__':
+    main()
