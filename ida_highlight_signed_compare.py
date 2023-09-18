2ee6add16897d412c6ded5046768b831c2ddf98e
VictorV <vv474172261@hotmail.com>
2023-09-18 18:06:04 +0800

    Update ida_highlight_signed_compare.py
    fix bug for cmov? condition

--- ida_highlight_signed_compare.py
+++ ida_highlight_signed_compare.py
@@ -1,9 +1,9 @@
 import idaapi
 import idc
 import ida_kernwin
+import idautils
 
 
-# Enable_Signed_TAG = True
 class MyPlugin(idaapi.plugin_t):
     flags = 0
     comment = "A plugin to highlight signed compare"
@@ -37,7 +37,6 @@ class MyPlugin(idaapi.plugin_t):
 
 class MyHooks(idaapi.Hexrays_Hooks):
     def double_click(self, vu, shift_state):
-        global Enable_Signed_TAG
         ea = vu.cfunc.entry_ea
         decompiler = idaapi.decompile(ea)
         pseudocode = decompiler.get_pseudocode()
@@ -51,21 +50,28 @@ class MyHooks(idaapi.Hexrays_Hooks):
                     if ret and pitem.it:
                         asm_ea = pitem.it.ea
                         asm_line = idc.GetDisasm(asm_ea)
-                        if any(op in asm_line for op in ['jg', 'jng', 'jl', 'jge', 'jle', 'jnl', 'js', 'jns', 'cmovg', 'cmovge', 'cmovl', 'cmovle', 'cmovs', 'cmovns']):
-                            # idaapi.tag_remove(line.line)
-                            # if Enable_Signed_TAG:
+                        # print(asm_line, hex(asm_ea))
+                        if 'cmp' in asm_line:# cmovl won't appear in this loop, so we need to check following assembly, I guess we can find jmp in 6 instructions.
+                            flag = 0
+                            for i in range(6):
+                                x3 = idautils.DecodeInstruction(asm_ea)
+                                asm_ea = x3.size + asm_ea
+                                asm_line = idc.GetDisasm(asm_ea)
+                                # print(asm_line, hex(asm_ea))
+                                if any(op in asm_line for op in ['jg', 'jng', 'jl', 'jge', 'jle', 'jnl', 'js', 'jns', 'cmovg', 'cmovge', 'cmovl', 'cmovle', 'cmovs', 'cmovns']):
+                                    flag = 1 # yes, we found condition jump
+                                    break
+                                elif any(op in asm_line for op in ['ja', 'jb', 'jz', 'jnz', 'cmove', 'cmovne', 'cmova', 'cmovae', 'cmovb', 'cmovbe', 'jae', 'jbe', 'jmp']):
+                                    break # if we find other jump, we assume that it's not signed jump. this may ignore some special cases.
+                            if flag:
                                 line.bgcolor = 0x55ff55 # green
                                 print("highlighted 1 line")
                                 break
-                        # else:
-                            # idaapi.tag_remove(line.line)
-                            # print("remove 1 line tag")
-        # if Enable_Signed_TAG:
-        #     print("diable tag")
-        #     Enable_Signed_TAG = False
-        # else:
-        #     print("enable tag")
-        #     Enable_Signed_TAG = True
+                        else:
+                            if any(op in asm_line for op in ['jg', 'jng', 'jl', 'jge', 'jle', 'jnl', 'js', 'jns', 'cmovg', 'cmovge', 'cmovl', 'cmovle', 'cmovs', 'cmovns']):
+                                    line.bgcolor = 0x55ff55 # green
+                                    print("highlighted 1 line")
+                                    break
         print("signed highlight")
         ida_kernwin.refresh_idaview_anyway()
         return 0
